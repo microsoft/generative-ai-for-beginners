d474d292c3b2b6f2f7c756ab393868a4b2fecdfa
Nitya Narasimhan <nitya@users.noreply.github.com>
2023-09-26 23:21:54 -0400

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Install OpenAI and Dotenv for Python
+# TODO: Check why this can't be done in requirements.txt
+pip install python-dotenv
+pip install openai
+
 # Install the OpenAI packages for Node.js
 # (Python related dependencies are covered in requirements.txt)
 # echo "Installing OpenAI For Node.js" 
