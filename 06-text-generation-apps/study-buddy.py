8759ed3b1813a10dd5cf8ed0f0dda2f701f22da4
Satheeshkumar Manoharan <11145865+infosatheesh2020@users.noreply.github.com>
2023-12-26 15:22:47 +0000

    Updated completion functions to use chat.completion

---
+++ 06-text-generation-apps/study-buddy.py
@@ -0,0 +1,39 @@
+from openai import AzureOpenAI
+import os
+import dotenv
+
+# import dotenv
+dotenv.load_dotenv()
+
+# configure Azure OpenAI service client 
+client = AzureOpenAI(
+  azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"], 
+  api_key=os.environ['AZURE_OPENAI_KEY'],  
+  api_version = "2023-10-01-preview"
+  )
+
+deployment=os.environ['AZURE_OPENAI_DEPLOYMENT']
+
+# add your completion code
+question = input("Ask your questions on python language to your study buddy: ")
+prompt = f"""
+You are an expert on the python language.
+
+Whenever certain questions are asked, you need to provide response in below format.
+
+- Concept
+- Example code showing the concept implementation
+- explanation of the example and how the concept is done for the user to understand better.
+
+Provide answer for the question: {question}
+"""
+messages = [{"role": "user", "content": prompt}]  
+# make completion
+completion = client.chat.completions.create(model=deployment, messages=messages)
+
+# print response
+print(completion.choices[0].message.content)
+
+#  very unhappy _____.
+
+# Once upon a time there was a very unhappy mermaid.
