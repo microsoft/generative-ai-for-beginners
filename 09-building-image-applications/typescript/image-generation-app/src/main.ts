072d311b1ead8699dc4fbd455c4ab19f101d2f0b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-16 01:50:09 +0000

    chore: Update image generation app configuration and code

--- 09-building-image-applications/typescript/image-generation-app/src/main.ts
+++ 09-building-image-applications/typescript/image-generation-app/src/main.ts
@@ -6,24 +6,25 @@ dotenv.config();
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT || '';
 const azureApiKey = process.env.AZURE_OPENAI_KEY || '';
 
+const promptImage = "captain with a parrot on his shoulder";
+
 export async function main() {
   try {
     console.log("== Image Generation App ==");
 
     const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-    const promptImage = "captain with a parrot on his shoulder";
-    const deploymentName = 'dall-e-3'; // the deployment should be Dall-E model
+    const deploymentName = 'dall-e-3';
 
     const imageGenerations = await client.getImages(deploymentName, promptImage, {
       n: 1,
       size: "1024x1024",
       responseFormat: "url",
       quality: "standard",
-      style: "natural",
+      style: "vivid",
     });
 
     for (const image of imageGenerations.data) {
-        console.log(`Image generated URL...: ${image.url}`);
+      console.log(`Image generated URL...: ${image.url}`);
     }
   } catch (error) {
     console.log("The sample encountered an error: ", error);
