a72d419c1fe33e638ae65cebec9503ec71434f93
KyryloChalov <ks7977166@gmail.com>
2025-04-26 19:24:43 +0000

    250426

---
+++ 00-course-setup/load_environment.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the GITHUB_TOKEN variable
+# github_token = os.getenv("GITHUB_TOKEN")
+
+# Print the environment variables
+print("Environment Variables:")
+print("GITHUB_TOKEN:    ", os.getenv("GITHUB_TOKEN"))
+print("GITHUB_REPO:     ", os.getenv("GITHUB_REPO"))
+print("GITHUB_USERNAME: ", os.getenv("GITHUB_USERNAME"))
